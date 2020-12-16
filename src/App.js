33bdaebbbeee6ef058b58a18f8d45f01ecab2953
Samuel Gashaw <samuelmgashaw@gmail.com>
2020-12-15 23:36:51 -0500

    the final home page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import './Header.css';
 import Header from './Header';
 import Home from './Home';
-
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 function App() {
   return (
-    <div className="App">
-      <Header></Header>
-      <Home></Home>
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/" exact>
+            <Header></Header>
+            <Home></Home>
+          </Route>
+          <Route path="/checkout">
+            <h1>I am a checkpout page</h1>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
--- src/Product.jsx
+++ src/Product.jsx
@@ -1,25 +1,24 @@
 import React from 'react';
 import './Product.css';
-function Product() {
+function Product({ title, image, price, rating }) {
   return (
     <div className="product">
       <div className="product__info">
-        <p>The lean startup</p>
+        <p>{title}</p>
         <p className="product__price">
           <small>$</small>
-          <strong>19.99</strong>
+          <strong>{price}</strong>
         </p>
         <div className="product__rating">
-          <p>🌟</p>
+          {Array(rating)
+            .fill()
+            .map((_, i) => (
+              <p>🌟</p>
+            ))}
         </div>
-
-        <img
-          src="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_BO1,204,203,200_.jpg"
-          alt=""
-        />
-
-        <button>Add to Basket</button>
       </div>
+      <img src={image} alt="" />
+      <button>Add to Basket</button>
     </div>
   );
 }
