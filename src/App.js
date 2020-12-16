7af890eb569aab1cf04ffcf19ebd9ef273654af1
Samuel Gashaw <samuelmgashaw@gmail.com>
2020-12-15 22:19:46 -0500

    home and product page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './Header.css';
 import Header from './Header';
+import Home from './Home';
+
 function App() {
   return (
     <div className="App">
-      {/*Header*/}
       <Header></Header>
-      {/*Home*/}
+      <Home></Home>
     </div>
   );
 }
